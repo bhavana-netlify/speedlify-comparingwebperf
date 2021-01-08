db024bb355e43b7d73c951ebbbd9053f54e06d51
bhavana-netlify <42786844+bhavana-netlify@users.noreply.github.com>
2021-01-08 16:25:02 -0500

    Create customer-sites.js

---
+++ _data/sites/customer-sites.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "Sample", // optional, falls back to object key
+	description: "The default sites that get tested",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://us.nexxus.com/us/en/"
+	]
+};
